84d8369dd4613a848cdf7cfae2d3e3cab924a384
mihail.coscodan <mihail.coscodan@dassolutions.eu>
2021-02-05 19:41:53 +0200

    Generate student.xml

--- XMLSample/Program.cs
+++ XMLSample/Program.cs
@@ -15,10 +15,34 @@ namespace XMLSample
         private static void GenerateXML()
         {
             // Generate Xml using XmlTextWriter
-            XmlTextWriter xmlTextWriter = new XmlTextWriter(@"C:\Users\Mihu\Desktop\XMLSample\student.xml", null);
+            XmlTextWriter xmlTextWriter = new XmlTextWriter(@"C:\Users\Mihu\Documents\XMLSample\XMLSample\student.xml", null);
+            xmlTextWriter.Formatting = Formatting.Indented;
             xmlTextWriter.WriteStartDocument();
             // add comments
             xmlTextWriter.WriteComment("My First comment in XML file");
+            xmlTextWriter.WriteStartElement("Student");
+            xmlTextWriter.WriteStartElement("r", "Record", "urn:record");
+
+            //write other element
+            xmlTextWriter.WriteStartElement("Name", "");
+            xmlTextWriter.WriteString("Student 1");
+            xmlTextWriter.WriteEndElement();
+
+            xmlTextWriter.WriteStartElement("Address", "");
+            xmlTextWriter.WriteString("Camin 2");
+            xmlTextWriter.WriteEndElement();
+
+            char[] chArray = new char[3];
+            chArray[0]='a';
+            chArray[1]='b';
+            chArray[2]='c';
+            xmlTextWriter.WriteStartElement("Char");
+            xmlTextWriter.WriteChars(chArray, 0, chArray.Length);
+            xmlTextWriter.WriteEndElement();
+            xmlTextWriter.WriteEndElement();
+            xmlTextWriter.Close();
+
+
         }
     }
 }
