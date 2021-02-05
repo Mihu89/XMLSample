b0ec3cbc31c173420bbf3508de65739d8a7a63c0
mihail.coscodan <mihail.coscodan@dassolutions.eu>
2021-02-05 18:53:31 +0200

    My First commit

---
+++ XMLSample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Xml;
+
+namespace XMLSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("XML Intro");
+            GenerateXML();
+
+        }
+
+        private static void GenerateXML()
+        {
+            // Generate Xml using XmlTextWriter
+            XmlTextWriter xmlTextWriter = new XmlTextWriter(@"C:\Users\Mihu\Desktop\XMLSample\student.xml", null);
+            xmlTextWriter.WriteStartDocument();
+            // add comments
+            xmlTextWriter.WriteComment("My First comment in XML file");
+        }
+    }
+}
---
+++ XMLSample/User.cs
@@ -0,0 +1,8 @@
+namespace XMLSample
+{
+     public class User{
+         public string Name { get; set; }
+         public int Age { get; set; }
+         public string Company { get; set; }
+     }
+}
